7300335c4691bd134378c698b9e02cc96228d226
petermbugua <pmbugua276@gmail.com>
2024-10-19 15:18:37 +0300

    refactored the codes

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Blog = () => {
   return (
-    <div className="min-h-screen bg-black text-white p-6 px-4 md:px-10">
+    <div className="min-h-screen bg-black text-white p-6 px-4 md:px-10 lg:px-20 xl:px-32">
       {/* Introduction/Author Bio */}
       <section className="mb-12">
         <h1 className="text-3xl font-bold mb-4 text-yellow-500">Peter Mbugua</h1>
