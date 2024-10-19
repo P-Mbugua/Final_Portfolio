b66658eba05a4636534118fb819cc8594239a2b2
petermbugua <pmbugua276@gmail.com>
2024-10-19 14:17:33 +0300

    refactored the codes

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getFirestore } from "firebase/firestore"; 
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAFey9M3UNzqKEk5190WydMz-ttMoiW6jU",
   authDomain: "portfolio1-eddb9.firebaseapp.com",
@@ -14,14 +13,14 @@ const firebaseConfig = {
   measurementId: "G-YK4YDN5E1Z"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
+
 const db = getFirestore(app);
 
-// Initialize Analytics
+
 const analytics = getAnalytics(app);
 
-// Export the db instance
+
 export { db };
