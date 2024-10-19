5b5cb03bb70ff5f1f3db8f6637aa02bdefa1009b
petermbugua <pmbugua276@gmail.com>
2024-10-19 13:22:03 +0300

    refactored the codes

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -89,7 +89,7 @@ export default function Skills() {
         </div>
       </div>
 
-      {/* Soft Skills Section */}
+      {/* Soft Skills Section*/}
       <div className="mt-16 w-full max-w-6xl">
         <h3 className="text-2xl font-bold text-green-500 mb-4">Soft Skills</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
